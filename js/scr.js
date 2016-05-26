2f68a497e474abbe424336a3ded697220449a366
olikerka <olikerka@gmail.com>
2016-05-26 09:33:50 +0300

    newbase

--- js/scr.js
+++ js/scr.js
@@ -1,9 +1,30 @@
  $(document).ready(function() {
-    $("#lightSlider").lightSlider({
+    $("#slider1").lightSlider({
     	item : 1,
         slideMargin: 0,
-    	
- 
+    	pager:	false,
+    	slideMove: 1,
+    	slideMargin: 2,
+ 		auto:true,
+        loop:true,
+        pauseOnHover: true,
+        onBeforeSlide: function (el) {
+            $('#current').text(el.getCurrentSlideCount());
+        } 
+       
+    }); 
+  });
+ $(document).ready(function() {
+    $("#slider2").lightSlider({
+    	item : 1,
+        slideMargin: 0,
+    	pager:	true,
+ 		auto:true,
+        loop:true,
+        pauseOnHover: true,
+        onBeforeSlide: function (el) {
+            $('#current').text(el.getCurrentSlideCount());
+        } 
        
     }); 
   });
